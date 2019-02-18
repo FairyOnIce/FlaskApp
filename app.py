db4874c12b5c0d4025b8fa60539605b6fc7b3cde
Yumi Kondo <yumik091186@gmail.com>
2019-02-17 16:19:08 -0800

    make it better

--- app.py
+++ app.py
@@ -1,43 +1,33 @@
-from flask import Flask, render_template, redirect, url_for, request
-import backend as be
-from backend import placeholder
-# Reference:
-# https://pythonspot.com/flask-web-app-with-python/
-# https://www.tutorialspoint.com/index.html
-
-## Heroku reference
-# https://github.com/datademofun/heroku-basic-flask
-
-# https://simple-text-prediction.herokuapp.com/
+from flask import Flask, render_template
+import sqlite3 as sql
+from prepare_data import TABLENAME
 app = Flask(__name__)
-pr = be.preprocess("sentiment")
+
 
 @app.route("/")
 def index():
-    output = None
-    return render_template(
-        "index_page.html",**locals()
-    )
+    return render_template("main.html", name="working mom")
+
+
+@app.route("/hello/<string:name>")
+def main(name):
+    return render_template("main.html",name=name)
 
 
 
-@app.route('/index_page',methods = ['POST', 'GET'])
-def respondToPostRequest():
-   if request.method == 'POST':
-      try:
-          textFromWeb = request.form['nm']
-          out = pr.predict(textFromWeb)
-          return redirect(url_for('show_result',result = out))
-      except:
-          return("unexpected error!")
-   return("Post reques is expected!")
+@app.route("/daycare_table")
+def daycare_table():
+    con = sql.connect("database.db")
+    con.row_factory = sql.Row
 
-@app.route('/out/<string:result>')
-def show_result(result):
-    input, output = result.split(placeholder)
-    return render_template(
-        "index_page.html",**locals())
+    cur = con.cursor()
+    cur.execute("select * from {}".format(TABLENAME))
+
+    rows = cur.fetchall();
+    column_names = rows[0].keys()
+    return render_template("list.html", rows=rows, column_names=column_names)
+
 
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0')
+    app.run(debug=True)
